aa96a01a0c16af6b1a9d577b76745f2df54238e4
royscofield <a11718trannhuy@gamil.com>
2022-06-06 18:05:53 +0700

    router and layout

---
+++ src/components/Layout/HeaderOnly/Header/index.js
@@ -0,0 +1,5 @@
+function HeaderOnly() {
+    return <h1>HeaderOnly</h1>;
+}
+
+export default HeaderOnly;
---
+++ src/pages/Upload copy/index.js
@@ -0,0 +1,8 @@
+function Search() {
+    return (
+        <div>
+            <h1>Search page</h1>
+        </div>
+    );
+}
+export default Search;
